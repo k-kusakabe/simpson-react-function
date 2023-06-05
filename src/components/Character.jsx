aa2489a50e405a2de199500ff1898292acaea384
k-kusakabe <k.kusa0828@gmail.com>
2023-06-05 20:37:43 +0100

    characters are displayed in functional

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,29 +1,54 @@
-import React, { Component } from "react";
+import React from "react";
 import Name from "./Name";
 import Quote from "./Quote";
 import Image from "./Image";
-import Delete from "./Delete";
 
-class Character extends Component {
-  render() {
-    const { character, quote, image, id, characterDirection, liked } =
-      this.props.item;
-    const { onLikeToggle, onDelete } = this.props;
+const Character = (props) => {
+  const { character, quote, image, id, characterDirection, liked } = props.item;
 
-    return (
-      <div className="characterContainer">
-        <Name
-          character={character}
-          onLikeToggle={onLikeToggle}
-          id={id}
-          liked={liked}
-        />
-        <Quote quote={quote} />
-        <Image image={image} />
-        <Delete onDelete={onDelete} id={id} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="characterContainer">
+      <Name
+        character={character}
+        // onLikeToggle={onLikeToggle}
+        // id={id}
+        // liked={liked}
+      />
+      <Quote quote={quote} />
+      <Image image={image} character={character} />
+      {/* <Delete onDelete={onDelete} id={id} /> */}
+    </div>
+  );
+};
 
 export default Character;
+
+// import React, { Component } from "react";
+// import Name from "./Name";
+// import Quote from "./Quote";
+// import Image from "./Image";
+// import Delete from "./Delete";
+
+// class Character extends Component {
+//   render() {
+//     const { character, quote, image, id, characterDirection, liked } =
+//       this.props.item;
+//     const { onLikeToggle, onDelete } = this.props;
+
+//     return (
+//       <div className="characterContainer">
+//         <Name
+//           character={character}
+//           onLikeToggle={onLikeToggle}
+//           id={id}
+//           liked={liked}
+//         />
+//         <Quote quote={quote} />
+//         <Image image={image} />
+//         <Delete onDelete={onDelete} id={id} />
+//       </div>
+//     );
+//   }
+// }
+
+// export default Character;
--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -1,18 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Name extends Component {
-  render() {
-    const { liked, character, onLikeToggle, id } = this.props;
-
-    return (
-      <div>
-        <h1>{character}</h1>
-        <button onClick={() => onLikeToggle(id)}>
-          {liked ? "Liked" : "Not liked"}
-        </button>
-      </div>
-    );
-  }
-}
+const Name = (props) => {
+  const { character } = props;
+  return <h1>{character}</h1>;
+};
 
 export default Name;
+
+// import React, { Component } from "react";
+
+// class Name extends Component {
+//   render() {
+//     const { liked, character, onLikeToggle, id } = this.props;
+
+//     return (
+//       <div>
+//         <h1>{character}</h1>
+//         <button onClick={() => onLikeToggle(id)}>
+//           {liked ? "Liked" : "Not liked"}
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Name;
